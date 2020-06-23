d0a4b6a90f8cbcb40601bba3358ebf550ea661e7
Soohyun <33894603+1211sh@users.noreply.github.com>
2020-06-24 03:14:57 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,44 @@
 #include "stm32l476xx.h"
 #include "SysClock.h"
-#include "EXTI.h"
 #include "LED.h"
+#include "UART.h"
 
-// Push joystick center and toggle RED LED
+#include <string.h>
+#include <stdio.h>
+
+char RxComByte = 0;
+uint8_t buffer[BufferSize];
+
 int main(void){
+	char rxByte;
+	int a, n, i;
+	float b;
+	
 	System_Clock_Init(); // Switch System Clock = 80 MHz
 	LED_Init();
-	Red_LED_Toggle();
-	EXTI_Init();
-	while(1);
+	UART2_Init();
+	
+	
+		
+	while (1){
+//		n = sprintf((char *)buffer, "a = %d\t", a);
+//		n += sprintf((char *)buffer + n, "b = %f\r\n", b);
+//		USART_Write(USART2, buffer, n);		
+//		a = a + 1;
+//		b = (float)a/100;
+//		for (i = 0; i < 8000000; i++); // Delay
+//		Red_LED_Toggle();
+		
+		printf("Give Red LED control input (Y = On, N = off):\r\n");
+		scanf ("%c", &rxByte);
+		if (rxByte == 'N' || rxByte == 'n'){
+			Red_LED_Off();
+			printf("LED is Off\r\n\r\n");
+		}
+		else if (rxByte == 'Y' || rxByte == 'y'){
+			Red_LED_On();
+			printf("LED is On\r\n\r\n");
+		}
+	}
 }
 
